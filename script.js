f8b8f6f6f444f72cd60f003749e58b38eb10239d
Anuj3688 <anuj.10tiwari2001@gmail.com>
2022-02-07 23:40:23 +0530

    First edition

---
+++ script.js
@@ -0,0 +1,15 @@
+function displayoff() {
+    console.log("Runnning");
+    document.getElementById("loginbakwas").style = 'display:none';
+}
+
+function displayshow() {
+    console.log("Runnnins");
+    document.getElementById("loginbakwas").style = 'display:block';
+}
+
+// Validation
+
+function validate() {
+    
+}
