6c0bca9e4a5a8efbed950b4aac77042a790cdb2d
Anuj3688 <anuj.10tiwari2001@gmail.com>
2022-02-08 23:34:27 +0530

    Added some style in the login page

--- script.js
+++ script.js
@@ -10,6 +10,40 @@ function displayshow() {
 
 // Validation
 
-function validate() {
+// function validate() {
     
+// }
+
+function passvisible(){
+    // var x = document.getElementById("pass");
+    if(document.getElementById("pass").type == "password")
+    document.getElementById("pass").type = "text";
+    else
+    document.getElementById("pass").type = "password";
+}
+
+function displaycrop(){
+    removeplant()
+    document.getElementById("cropdetails").style = "display:grid";
+    document.getElementById("navhead").style = " background-color: #85FFBD ";
+    document.getElementById("navhead").style = "background-image: linear-gradient(45deg, #85FFBD 0%, #FFFB7D 100%)";
+}
+
+function removecrop(){
+    document.getElementById("navhead").style = "background-image:none";
+    document.getElementById("navhead").style = "background-color: #FBF8CC";
+    document.getElementById("cropdetails").style = "display:none";
+}
+
+function displayplant(){
+    removecrop()
+    document.getElementById("plantdetails").style = "display:grid";
+    document.getElementById("navhead").style = " background-color: #85FFBD ";
+    document.getElementById("navhead").style = "background-image: linear-gradient(45deg, #85FFBD 0%, #FFFB7D 100%)";
+}
+
+function removeplant(){
+    document.getElementById("navhead").style = "background-image:none";
+    document.getElementById("navhead").style = "background-color: #FBF8CC";
+    document.getElementById("plantdetails").style = "display:none";
 }
